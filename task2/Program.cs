fdd6305cd773cb79653b5e7c1eba50ac1eecb34e
akifnihat <akifst@code.edu.az>
2024-02-15 14:34:27 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Net.Http.Headers;
+
+Console.WriteLine("Hello, World!");
+#region task1
+//int y, x, count;
+//y = 10;
+//x = 60;
+//count = 0;
+//for (int i = y; i <= x; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);    
+#endregion
+
+#region task2
+//int a = 80;
+//if (a % 2 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (a % 3 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (a % 4 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (a % 5 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (a % 7 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if(a % 2 == 1)
+//{
+//    Console.WriteLine("sade");
+//}
+#endregion
+
+#region task3
+//int a = 15;
+//int b = 5;
+//if (a % b == 0)
+//{
+//    Console.WriteLine("tam bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+#endregion
+
+#region task4
+//int a = 20;
+//int b = 30;
+//int c = 50;
+//if (a < c && a < b)
+//{
+//    console.writeline(b + c);
+//}
+//else
+//{
+//    console.writeline("duzgun eded daxil et");
+//}
+#endregion
